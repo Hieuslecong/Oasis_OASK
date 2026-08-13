845aa1ca54c544310f3755b3f88d200c1c7c6d1a
Hieus.lecong <85163251+Hieuslecong@users.noreply.github.com>
2026-08-13 23:54:33 +0700

    chore: bump method-contract implementation

--- src/oasis_rc_v2/checkpoint.py
+++ src/oasis_rc_v2/checkpoint.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 EXPERIMENT_ID = "oasis-rc-v2-relational-hard-negative"
 CHECKPOINT_SCHEMA = 2
-IMPLEMENTATION_VERSION = "2.0.1"
+IMPLEMENTATION_VERSION = "2.0.2"
 METHOD_VERSION = "OASIS-RC-v2"
 
 
